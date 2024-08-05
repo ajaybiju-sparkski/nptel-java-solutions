923e089ff6377b06838ca3a0e830c8c5ed406d59
ajaybiju-sparkski <164858553+ajaybiju-sparkski@users.noreply.github.com>
2024-08-06 04:46:35 +0530

    hotel w1assignment4.java
    to make such a pattern like a right angle triangle with the number increased by 1

---
+++ w1assignment4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class W01_P4 {
+
+  public static void main(String[] args)
+
+  {
+    int i, j, n, k = 1;
+
+    //System.out.print("Input number of rows : ");
+
+    Scanner in = new Scanner(System.in);
+    n = in.nextInt();
+
+  for (i = 1; i <= n; i++){
+  for (j = 1; j <= 1; j++){
+    System.out.print(k + " ");
+    k++;
+  }
+  System.out.println();
+}
+
+}
+}
