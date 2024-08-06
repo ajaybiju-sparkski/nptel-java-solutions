5b5797f9c864721340e66f8ac288a7ac05bed46e
ajaybiju-sparkski <164858553+ajaybiju-sparkski@users.noreply.github.com>
2024-08-06 19:33:48 +0530

    nptel w3assignment1.java
    Create a class Department having a method getCourses that prints "These are the department's courses". It will have two subclasses, ComputerScience and MechanicalEngineering, each having a method with the same name that prints specific courses for the respective departments.Call the method by creating an object of each of the three classes.

---
+++ w3assignment1.java
@@ -0,0 +1,26 @@
+class Department {
+  public void getCourses() {
+    System.out.println("These are the department's courses");
+  }
+}
+
+class ComputerScience extends Department{
+  public void getCourses() {
+    System.out.println("Courses: Data Structures, Algorithms, Operating Systems");
+  }
+}
+ 
+class MechanicalEngineering extends Department{
+  public void getCourses() {
+    System.out.println("Courses: Thermodynamics, Fluid Mechanics, Heat Transfer");
+  }
+}
+
+lass W03_P1 {
+  public static void main(String[] args) {
+    ComputerScience cs = new ComputerScience();
+    MechanicalEngineering me = new MechanicalEngineering();
+    cs.getCourses();
+    me.getCourses();
+  }
+}
