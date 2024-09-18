d26306197eb486ed152d16b24c93a0c8dc8cc154
ajaybiju-sparkski <164858553+ajaybiju-sparkski@users.noreply.github.com>
2024-09-18 22:03:17 +0530

    nptel w8assignment1.java

---
+++ w8assignment1.java
@@ -0,0 +1,11 @@
+public class W08_P1 { 
+	public static void main(String args[]){
+  java.util.Calendar current;
+
+  current = java.util.Calendar.getInstance();
+
+  year = current.get(current.YEAR);
+System.out.println("Current Year: "+year);
+  ~~~THERE IS SOME INVISIBLE CODE HERE~~~
+  }
+}
