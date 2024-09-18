91cfaea1ce52d9b0083ba0e8377d735f836775c5
ajaybiju-sparkski <164858553+ajaybiju-sparkski@users.noreply.github.com>
2024-09-18 22:15:07 +0530

    nptel w8assignment4.java

---
+++ w8assignment4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import static java.lang.System.out;
+
+public class W08_P4{
+  public static void main(String[] args) {
+	// Scanner object is created
+    Scanner scanner = new Scanner(System.in);
+     // Read String input using scanner class
+    String courseName = scanner.nextLine(); 
+     // Print the scanned String
+    out.print("Course: " + courseName); 
+  }
+}
