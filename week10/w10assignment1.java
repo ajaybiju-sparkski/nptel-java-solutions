cc27ee50f6bb9a884d63cc246f91728f870976e2
ajaybiju-sparkski <164858553+ajaybiju-sparkski@users.noreply.github.com>
2024-10-02 21:52:25 +0530

    nptel w10assignment1.java
    Complete the code fragment to read three integer inputs from keyboard and find the sum and store the result in the variable "sum".
    NOTE: Name the class W10_P1 and the file W10_P1.java

---
+++ week10/w10assignment1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+        public class W10_P1{ 
+        public static void main (String[] args){ 
+            
+             int i,n=0,sum=0;
+             Scanner inr = new Scanner(System.in);
+        for (i=0;i<3;i++)
+        {
+            n = inr.nextInt();
+            
+        sum =sum+n;
+            }
+System.out.print(sum);
+  }
+}
